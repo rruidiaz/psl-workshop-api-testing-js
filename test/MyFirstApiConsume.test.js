6d66fe35b644e5fbbfb608cf49ab519556b87b87
rruidiaz <rruidiaz8@gmail.com>
2017-09-24 17:24:00 -0500

    eslint setup and corrections

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -2,15 +2,13 @@ const agent = require('superagent-promise')(require('superagent'), Promise);
 const statusCode = require('http-status-codes');
 const chai = require('chai');
 
-const expect = chai.expect;
+const { expect } = chai;
 
 describe('First Api Tests', () => {
-  it('Consume GET Service', () => {
-    return agent.get('https://httpbin.org/ip').then((response) => {
-      expect(response.status).to.equal(statusCode.OK);
-      expect(response.body).to.have.property('origin');
-    });
-  });
+  it('Consume GET Service', () => agent.get('https://httpbin.org/ip').then((response) => {
+    expect(response.status).to.equal(statusCode.OK);
+    expect(response.body).to.have.property('origin');
+  }));
 
   it('Consume GET Service with query parameters', () => {
     const query = {
@@ -52,7 +50,6 @@ describe('First Api Tests', () => {
       .query(query)
       .then((response) => {
         expect(response.status).to.equal(statusCode.OK);
-        expect(response.body).to.be.empty;
       });
   });
 
