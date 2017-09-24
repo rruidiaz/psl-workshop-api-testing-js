e2c137b14aa2d1ebd7343762b390d663894af231
rruidiaz <32225345+rruidiaz@users.noreply.github.com>
2017-09-23 21:27:09 -0500

    Api test superagent (#2)
    * Configuración dependencias http, superagent
    
    * Pruebas de api consumiendo servicios HEAD, PATCH, PUT, DELETE
    
    * Se elimina HelloWord Test

---
+++ test/MyFirstApiConsume.test.js
@@ -0,0 +1,103 @@
+const agent = require('superagent-promise')(require('superagent'), Promise);
+const statusCode = require('http-status-codes');
+const chai = require('chai');
+
+const expect = chai.expect;
+
+describe('First Api Tests', () => {
+  it('Consume GET Service', () => {
+    return agent.get('https://httpbin.org/ip').then((response) => {
+      expect(response.status).to.equal(statusCode.OK);
+      expect(response.body).to.have.property('origin');
+    });
+  });
+
+  it('Consume GET Service with query parameters', () => {
+    const query = {
+      name: 'John',
+      age: '31',
+      city: 'New York'
+    };
+    return agent.get('https://httpbin.org/get')
+      .query(query)
+      .then((response) => {
+        expect(response.status).to.equal(statusCode.OK);
+        expect(response.body.args).to.eql(query);
+      });
+  });
+
+  it('Consume POST Service', () => {
+    const body = {
+      name: 'John',
+      age: 31,
+      city: 'New York'
+    };
+
+    return agent
+      .post('https://httpbin.org/post')
+      .send(body)
+      .then((response) => {
+        expect(response.status).to.equal(statusCode.OK);
+        expect(response.body.json).to.eql(body);
+      });
+  });
+
+  it('Consume HEAD Service with query parameters', () => {
+    const query = {
+      name: 'John',
+      age: '31',
+      city: 'New York'
+    };
+    return agent.head('https://httpbin.org/get')
+      .query(query)
+      .then((response) => {
+        expect(response.status).to.equal(statusCode.OK);
+        expect(response.body).to.be.empty;
+      });
+  });
+
+  it('Consume PATCH Service', () => {
+    const body = {
+      name: 'John',
+      age: 31,
+      city: 'New York'
+    };
+
+    return agent
+      .patch('https://httpbin.org/patch')
+      .send(body)
+      .then((response) => {
+        expect(response.status).to.equal(statusCode.OK);
+        expect(response.body.json).to.eql(body);
+      });
+  });
+
+  it('Consume PUT Service', () => {
+    const body = {
+      name: 'John',
+      age: 31,
+      city: 'New York'
+    };
+
+    return agent
+      .put('https://httpbin.org/put')
+      .send(body)
+      .then((response) => {
+        expect(response.status).to.equal(statusCode.OK);
+        expect(response.body.json).to.eql(body);
+      });
+  });
+
+  it('Consume Delete Service with query parameters', () => {
+    const query = {
+      name: 'John'
+    };
+    return agent.del('https://httpbin.org/delete')
+      .query(query)
+      .then((response) => {
+        expect(response.status).to.equal(statusCode.OK);
+        expect(response.body.args).to.eql(query);
+      });
+  });
+});
+
