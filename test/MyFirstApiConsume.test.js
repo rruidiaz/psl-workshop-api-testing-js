6415fb49ab74e517f5acbd3a6c9e4d38e1026c1c
rruidiaz <32225345+rruidiaz@users.noreply.github.com>
2017-09-25 17:43:49 -0500

    Eslint setup (#6)
    * eslint dependencies
    * eslint setup and corrections

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -2,15 +2,15 @@ const agent = require('superagent-promise')(require('superagent'), Promise);
 const statusCode = require('http-status-codes');
 const chai = require('chai');
 
-const expect = chai.expect;
+const { expect } = chai;
+const { assert } = chai;
+
 
 describe('First Api Tests', () => {
-  it('Consume GET Service', () => {
-    return agent.get('https://httpbin.org/ip').then((response) => {
-      expect(response.status).to.equal(statusCode.OK);
-      expect(response.body).to.have.property('origin');
-    });
-  });
+  it('Consume GET Service', () => agent.get('https://httpbin.org/ip').then((response) => {
+    expect(response.status).to.equal(statusCode.OK);
+    expect(response.body).to.have.property('origin');
+  }));
 
   it('Consume GET Service with query parameters', () => {
     const query = {
@@ -52,7 +52,7 @@ describe('First Api Tests', () => {
       .query(query)
       .then((response) => {
         expect(response.status).to.equal(statusCode.OK);
-        expect(response.body).to.be.empty;
+        assert.isEmpty(response.body);
       });
   });
 
