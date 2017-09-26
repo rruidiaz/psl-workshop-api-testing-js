c8d9882d3550b37afe4b669e6893ccf8f16d8139
rruidiaz <rruidiaz8@gmail.com>
2017-09-25 20:26:24 -0500

    Correcciones  eslint

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -1,7 +1,3 @@
-/*
-eslint linebreak-style: ["error", "windows"]
-*/
-
 const agent = require('superagent-promise')(require('superagent'), Promise);
 const statusCode = require('http-status-codes');
 const chai = require('chai');
